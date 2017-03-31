45f9a1508a4f140dcde3d254f0b407d214eae438
Sam <sam210723@hotmail.com>
2017-03-31 13:02:17 +1100

    ESP init.lua

---
+++ ESP8266/init.lua
@@ -0,0 +1,8 @@
+-- ESP-CC1101 https://github.com/sam210723/ESP-CC1101
+-- init.lua
+
+-- Configure UART for 9600 baud (NodeMCU default = 115200)
+uart.setup(0, 9600, 8, 0, 1, 1) -- id, baud, databits, parity, stopbits, echo
+
+print("\nNODEMCU_READY") -- Notify Arduino of successful boot
+dofile("main.lua")
