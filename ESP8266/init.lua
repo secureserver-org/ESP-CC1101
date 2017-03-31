44082ef4fa05dd061634d476895bbcd9a42bc20f
Sam <sam210723@hotmail.com>
2017-03-31 13:03:01 +1100

    Add UART delay

--- ESP8266/init.lua
+++ ESP8266/init.lua
@@ -3,6 +3,7 @@
 
 -- Configure UART for 9600 baud (NodeMCU default = 115200)
 uart.setup(0, 9600, 8, 0, 1, 1) -- id, baud, databits, parity, stopbits, echo
+tmr.delay(250 * 1000) -- Wait 250ms for UART
 
 print("\nNODEMCU_READY") -- Notify Arduino of successful boot
 dofile("main.lua")
